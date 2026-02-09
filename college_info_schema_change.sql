043460be17abdb566f6fcbea2d13f20077e47734
GouthamA15 <lucky.masna1@gmail.com>
2026-02-09 16:02:07 +0530

    deleted

--- college_info_schema_change.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE `college_info`
-DROP COLUMN `first_sem_start_date`,
-DROP COLUMN `second_sem_start_date`,
-ADD COLUMN `first_sem_start_month` TINYINT DEFAULT NULL AFTER `id`,
-ADD COLUMN `first_sem_start_day` TINYINT DEFAULT NULL AFTER `first_sem_start_month`,
-ADD COLUMN `second_sem_start_month` TINYINT DEFAULT NULL AFTER `first_sem_start_day`,
-ADD COLUMN `second_sem_start_day` TINYINT DEFAULT NULL AFTER `second_sem_start_month`;
