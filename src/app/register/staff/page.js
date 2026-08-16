21b2fb07d3cd8507209e3e090bd3b1d363b4585c
P.Sannith <88277591+Sannith-Hack@users.noreply.github.com>
2026-08-16 20:17:26 +0530

    Just a picture

--- src/app/register/staff/page.js
+++ src/app/register/staff/page.js
@@ -63,7 +63,7 @@ export default function StaffRegistrationPage() {
         <div className="max-w-5xl mx-auto px-4 py-4 flex flex-col md:flex-row items-center justify-between gap-4">
           <div className="flex items-center gap-3">
             <Image
-              src="/ku-logo.png"
+              src="assets\ku-college-logo.png"
               alt="KUCET Emblem"
               width={56}
               height={56}
